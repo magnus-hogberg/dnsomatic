a2b9ca05afb84847b9816ccf61d1fed63db5781f
Joseph Greenawalt <joegreenawalt@startmail.com>
2015-09-26 17:19:40 -0400

    some logging improvements

--- dnsomatic.py
+++ dnsomatic.py
@@ -1,20 +1,36 @@
 import requests, time, os
-import logging
+import logging, logging.config
 
-logging.basicConfig()
-log = logging.getLogger("dockerdnsomatic")
-log.addFilter(logging.Filter(name="dockerdnsomatic"))
-log.setLevel(logging.INFO)
+logging.config.fileConfig('logging.conf')
+
+# create logger
+log = logging.getLogger('omatic')
+
+# logging.basicConfig(format='%(asctime)s|%(levelname)s|%(message)s')
+# log = logging.getLogger("dockerdnsomatic")
+# log.addFilter(logging.Filter(name="dockerdnsomatic"))
+# log.setLevel(logging.INFO)
 
 username=os.getenv("MATICUSERNAME")
 password=os.getenv("MATICPASSWORD")
 lapse=float(os.getenv("MATICLAPSE"))
+ip=""
 
+def set_ip(text):
+    global ip
+    new_ip = text.rsplit()[1]
+    if new_ip != ip:
+        ip = new_ip
+        log.info("NEW_IP:" + new_ip)
 
 while True:
     r = requests.get('https://updates.dnsomatic.com/nic/update', auth=(username, password))
-    status = r.status_code
-    log.info(status)
-    if status != 200: 
+    if r.status_code != 200:
+        log.error(r.headers)
         log.error(r.text)
+    else:
+        log.info(r.text)
+        set_ip(r.text)
+
     time.sleep( lapse )
+
